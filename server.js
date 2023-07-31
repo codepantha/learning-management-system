db672cf06f09a8d4a00bb2a4164b36b51509b25a
codepanther <pezechigozirim@gmail.com>
2023-07-31 06:20:26 +0100

    Connect application to mongodb database

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
 const http = require('http');
+require('./config/dbConnect');
 const app = require("./app/app");
+
 
 const PORT = process.env.PORT || 8000;
 
 // server
 const server = http.createServer(app);
 server.listen(PORT, console.log(`server started on port ${PORT}`));
+
+// DolrK9z2Qd35QyNI
