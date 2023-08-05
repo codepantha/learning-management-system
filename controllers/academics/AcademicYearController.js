7466478a4b807a74a6fd94b1736b3558336732ba
codepanther <pezechigozirim@gmail.com>
2023-08-05 12:05:33 +0100

    Create endpoint to update an academic year

--- controllers/academics/AcademicYearController.js
+++ controllers/academics/AcademicYearController.js
@@ -1,5 +1,5 @@
-const CustomError = require("../../errors/CustomError");
-const AcademicYear = require("../../models/Academic/AcademicYear");
+const CustomError = require('../../errors/CustomError');
+const AcademicYear = require('../../models/Academic/AcademicYear');
 
 //@desc Get all Academic Years
 //@route GET /api/v1/academic-years
@@ -11,8 +11,8 @@ exports.index = async (req, res) => {
     status: 'success',
     message: 'Academic years fetched',
     data
-  })
-}
+  });
+};
 
 //@desc Get Single Academic Year
 //@route POST /api/v1/academic-years/:id
@@ -24,7 +24,7 @@ exports.show = async (req, res) => {
     message: 'Academic year fetched successfully',
     data
   });
-}
+};
 
 //@desc Create Academic Year
 //@route POST /api/v1/academic-years
@@ -37,11 +37,37 @@ exports.create = async (req, res) => {
 
   if (exists) throw new CustomError('Duplicate academic year.', 409);
 
-  const data = await AcademicYear.create({ name, fromYear, toYear, createdBy: req.userAuth._id });
+  const data = await AcademicYear.create({
+    name,
+    fromYear,
+    toYear,
+    createdBy: req.userAuth._id
+  });
 
   res.status(201).json({
     status: 'success',
     message: 'Academic year created successfully',
+    data
+  });
+};
+
+exports.update = async (req, res) => {
+  const { id } = req.params;
+  const { name, fromYear, toYear } = req.body;
+
+  const exists = await AcademicYear.findOne({ name });
+
+  if (exists) throw new CustomError('Academic year already exists', 409);
+
+  const data = await AcademicYear.findByIdAndUpdate(
+    id,
+    { name, fromYear, toYear, createdBy: req.userAuth._id },
+    { new: true }
+  );
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Academic year updated successfully!',
     data
   });
 };
--- routes/academics/academicYearRouter.js
+++ routes/academics/academicYearRouter.js
@@ -1,9 +1,9 @@
-const { create, index, show } = require('../../controllers/academics/AcademicYearController');
+const { create, index, show, update } = require('../../controllers/academics/AcademicYearController');
 const isAdmin = require('../../middleware/isAdmin');
 
 const router = require('express').Router();
 
 router.route('/').get(isAdmin, index).post(isAdmin, create);
-router.route('/:id').get(isAdmin, show);
+router.route('/:id').get(isAdmin, show).put(isAdmin, update);
 
 module.exports = router;
