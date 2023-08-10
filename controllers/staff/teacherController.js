242b4adec11dde4aba97bcbfcbd1c0cd7cc96e63
codepanther <pezechigozirim@gmail.com>
2023-08-10 10:29:49 +0100

    Add feature for teachers to update their profiles

--- controllers/staff/teacherController.js
+++ controllers/staff/teacherController.js
@@ -1,6 +1,7 @@
 const CustomError = require('../../errors/CustomError');
 const Teacher = require('../../models/Staff/Teacher');
 const generateToken = require('../../utils/generateToken');
+const { hash_password } = require('../../utils/helpers');
 
 //@desc Register teacher
 //@route POST /api/v1/teachers/register
@@ -58,7 +59,7 @@ exports.index = async (req, res) => {
     message: 'Teachers fetched successfully.',
     data
   });
-}
+};
 
 //@desc Show teacher
 //@route GET /api/v1/teachers/:id
@@ -73,18 +74,60 @@ exports.show = async (req, res) => {
     status: 'success',
     message: 'Teacher fetched successfully.',
     data
-  })
-}
+  });
+};
+
+//@desc Show teacher profile
+//@route GET /api/v1/teachers/profile
+//@access Private loggedIn Teacher
 
 exports.profile = async (req, res) => {
-  console.log(req.userAuth._id)
-  const data = await Teacher.findById(req.userAuth._id).select('-password')
+  const data = await Teacher.findById(req.userAuth._id).select('-password');
 
   if (!data) throw new CustomError('teacher profile not found', 404);
-  
+
   res.status(200).json({
     status: 'success',
     message: 'Teacher profile fetched successfully.',
     data
-  })
-}
+  });
+};
+
+//@desc Update teacher
+//@route GET /api/v1/teachers/:id
+//@access Private Teacher
+
+exports.update = async (req, res) => {
+  const loggedInTeacherId = req.userAuth._id;
+
+  // check that the teacher is updating their own profile
+  if (!loggedInTeacherId.equals(req.params.id))
+    throw new CustomError(
+      'Access denied! You can only update your own profile.',
+      403
+    );
+
+  const { name, email } = req.body;
+
+  const emailExists = await Teacher.findOne({ email });
+
+  if (emailExists) throw new CustomError('Email already exists.', 409);
+
+  let { password } = req.body;
+
+  if (password) {
+    password = await hash_password(password);
+  }
+
+  const data = await Teacher.findByIdAndUpdate(
+    loggedInTeacherId,
+    { name, email, password },
+    { new: true, runValidators: true }
+  );
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Teacher updated successfully.',
+    data
+  });
+};
--- routes/staff/teacherRouter.js
+++ routes/staff/teacherRouter.js
@@ -1,4 +1,4 @@
-const { register, login, show, profile } = require('../../controllers/staff/teacherController');
+const { register, login, show, profile, update } = require('../../controllers/staff/teacherController');
 const isAdmin = require('../../middleware/isAdmin');
 const isLoggedIn = require('../../middleware/isLoggedIn');
 const isTeacher = require('../../middleware/isTeacher');
@@ -8,6 +8,6 @@ const router = require('express').Router();
 router.post('/register', isLoggedIn, isAdmin, register);
 router.post('/login', login)
 router.get('/profile', isLoggedIn, isTeacher, profile);
-router.route('/:id').get(isLoggedIn, show);
+router.route('/:id').get(isLoggedIn, show).put(isLoggedIn, isTeacher, update);
 
 module.exports = router;
